140821ace1dbd3e34019b3b08354fa4f34196280
chi-hsuan <chihsuan.huang.7@gmail.com>
2014-05-17 01:53:09 +0800

    add start posfix priorty funcion intercodeGerator to build three address code

--- lib/InterCodeGenerator.h
+++ lib/InterCodeGenerator.h
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <map>
+#include <queue>
+#include <stack>
+#include "Node.h"
+#include <vector>
 
 using namespace std;
 
@@ -9,10 +14,13 @@ class InterCodeGenerator{
 	public:
 		InterCodeGenerator();
 		~InterCodeGenerator();
-	
-		void threeAddressCode();
+		void startGenerate(multimap<int, Node> parsingTree);
 		void quadruples();
 
 	private:
+		void postfix(queue<Node> expr);
+		void threeAddressCode();
+		int priority(Node node);
+		vector<queue<string> > expr_v_queue;
 		void outputQuadruples();
 };
